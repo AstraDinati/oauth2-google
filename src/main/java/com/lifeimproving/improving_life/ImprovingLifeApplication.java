c33e65f58677c334fc0eef18d01a3129c2fb0ba0
vadim <bigulipa@gmail.com>
2024-06-10 01:07:54 +0400

    init

---
+++ src/main/java/com/lifeimproving/improving_life/ImprovingLifeApplication.java
@@ -0,0 +1,14 @@
+package com.lifeimproving.improving_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovingLifeApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovingLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifeimproving/improving_life/ImprovingLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeimproving.improving_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovingLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
